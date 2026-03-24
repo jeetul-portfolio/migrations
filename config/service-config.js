1115cb25e263a39a13d16e0b46a2e76fb62403fa
Jeetul Samaiya <jeetulsamaiya.js@gmail.com>
2026-03-25 00:19:44 +0530

    fix(config): update serviceName to 'migrations' in service-config.js and rename package to 'migrations' in package.json

--- config/service-config.js
+++ config/service-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  serviceName: process.env.SERVICE_NAME || 'default',
+  serviceName: process.env.SERVICE_NAME || 'migrations',
   logging : {
     isPretty: false, // Set to true for pretty JSON output in logs
   },
